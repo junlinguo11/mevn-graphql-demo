471ac514496306c97e3eef84a5ab6a392de06eaf
Junlin Guo <junlin.guo11@gmail.com>
2018-06-29 16:37:41 +0800

    apollo-boost

--- vue_src/src/graphql/graphqlClient.js
+++ vue_src/src/graphql/graphqlClient.js
@@ -1,8 +1,5 @@
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from "apollo-boost";
 
 export default new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
-  cache: new InMemoryCache(),
+  uri: "http://localhost:3000/graphql"
 });
